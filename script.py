3dc603655074bf9329616501e77d53b9dab8bd21
methlox <astroyellowh@gmail.com>
2024-02-27 01:04:10 +0530

    response and error handling

--- script.py
+++ script.py
@@ -7,7 +7,14 @@ import time
 import re
 
 # Headers
-my_header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
 # URL for Scraping
 url= 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.56&lng=73.95&restaurantId=<restaurant_id>'
+
+# Response
+response = requests.get(url, headers = headers)
+
+# Error Handling
+if response.status_code != 200:
+    raise Exception('Failed to load page {}'.format(url))
