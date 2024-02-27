75ceeaf8dd348290efccb45f752b7b0170e17e00
methlox <astroyellowh@gmail.com>
2024-02-27 14:04:29 +0530

    all the namess fetched succesfully

--- script.py
+++ script.py
@@ -1,17 +1,12 @@
 # Import Required Libraries
-from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import json
-import time
-import re
 
 # Headers
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
 # URL for Scraping
 url= 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.56&lng=73.95&restaurantId=37968'
-
 res=[]
 
 # Response
@@ -23,11 +18,16 @@ if response.status_code != 200:
 
 data=response.json()
 
-# for p in data['data']['cards'][4]['groupedCard']['cardGroupMap']['REGULAR']['cards'][1]['card']['card']['itemCards'][0]['card']['info']['name']:
-#     res.append(p)
-
-res.append(data['data']['cards'][4]['groupedCard']['cardGroupMap']['REGULAR']['cards'][1]['card']['card']['itemCards'][0]['card']['info']['name'])
-
-
+for c in range(1, 11):
+    for cards in data['data']['cards'][4]['groupedCard']['cardGroupMap']['REGULAR']['cards'][c]['card']['card']['itemCards']:
+        res.append(cards['card']['info']['name'])
+        
+for cards in data['data']['cards'][4]['groupedCard']['cardGroupMap']['REGULAR']['cards'][11]['card']['card']['categories']:
+    for items in cards['itemCards']:
+        res.append(items['card']['info']['name'])
+        
+for c in range(12, 17):
+    for cards in data['data']['cards'][4]['groupedCard']['cardGroupMap']['REGULAR']['cards'][c]['card']['card']['itemCards']:
+        res.append(cards['card']['info']['name'])
 
 print(res)
